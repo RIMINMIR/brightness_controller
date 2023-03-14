934aeb993c98284145a6347f042e780c49d5520a
Viktor <11111rr8@gmail.com>
2023-03-14 09:36:44 +0600

    add correct file save and loading

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,9 @@ class BrightnessController(ConanFile):
     exports_sources = "src/*"
     requires = (
         "libgd/2.3.0"
+        
     )
-
+    default_options = {"libgd:with_png": True}
     def build(self):
         cmake = CMake(self)
         cmake.configure()
--- image_file_manager.cpp
+++ image_file_manager.cpp
@@ -1,23 +1,36 @@
 #include "image_file_manager.h"
 
+#include <cstdio>
 #include <stdexcept>
 
 std::shared_ptr<ImageWrapper> ImageFileManager::LoadImage (std::string path)
 {
-    auto image = gdImageCreateFromFile(path.c_str());
-
-    if(!image)
+    FILE *in = fopen(path.c_str(),"rb");
+    if(!in)
     {
         throw std::logic_error("error while opening the image\n");
     }
-
+    auto image = gdImageCreateFromPng(in);
+    
+    if(!image)
+    {
+        fclose(in);
+        throw std::logic_error("error while loading the image\n");
+    }
+    fclose(in);
     return std::make_shared<ImageWrapper>(image);
 }
 
 void ImageFileManager::SaveImage(std::string path, std::shared_ptr<ImageWrapper> image)
 {
-    if(gdImageFile(image->Get(), path.c_str()))
+
+    FILE *out = fopen(path.c_str(),"wb");
+    
+    if(!out)
     {
         throw std::logic_error("error while saving the image\n");
     }
+    gdImagePng(image->Get(), out);
+
+    fclose(out);
 }
